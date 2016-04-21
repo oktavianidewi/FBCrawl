fcdd86a47bdbd77cb96a38cfa364155c2d0111d3
oktavianidewi <oktaviani.dewi@gmail.com>
2016-04-21 19:38:22 +0900

    ada fitur convert2csv

--- readJson.py
+++ readJson.py
@@ -2,13 +2,11 @@ import re
 import json
 import nltk
 from textblob import TextBlob, Blobber
-from nltk.corpus import stopwords
-from nltk.tokenize import TabTokenizer
 from textblob.taggers import NLTKTagger
 from textblob.sentiments import NaiveBayesAnalyzer
+import csv
 
-def getData():
-    nama_file = 'feedconstitutionalpatriot.json'
+def getData(nama_file):
     token = []
     with open(nama_file) as data_file:
         json_data = json.load(data_file)
@@ -29,8 +27,9 @@ def subj_polar(message):
     blob = TextBlob(message)
     polarity  = blob.sentiment.polarity
     subjectivity = blob.sentiment.subjectivity
-    result = 'SubjPolar(subjectivity=%s, polarity=%s)' %(subjectivity, polarity)
-    return result
+    # result = 'SubjPolar(subjectivity=%s, polarity=%s)' %(subjectivity, polarity)
+    # return result
+    return subjectivity, polarity
 
 def pos_tagger(message):
     nltk_tagger = NLTKTagger()
@@ -40,13 +39,41 @@ def pos_tagger(message):
 def advancedsentiment(message):
     # message = unicode(message, 'utf8')
     sent = TextBlob(message, analyzer=NaiveBayesAnalyzer())
-    result = sent.sentiment
-    return result
+    classification = sent.sentiment.classification
+    posvalue = sent.sentiment.p_pos
+    negvalue = sent.sentiment.p_neg
+    return classification, posvalue, negvalue
 
+token = getData('feedconstitutionalpatriot.json')
 
-token = getData()
-for message_no, message in enumerate(token[:10]):
+# write to csv file
+outfile = file('constitutionalpatriot.csv','wb')
+writer = csv.writer(outfile,delimiter=',',quoting=csv.QUOTE_MINIMAL)
+
+for message_no, message in enumerate(token):
     # messagenourl = removeURL(message)
-    print subj_polar(message)
-    print advancedsentiment(message)
+    """
+    subjectivity = subj_polar(message)[0]
+    polarity = subj_polar(message)[1]
+    classification = advancedsentiment(message)[0]
+    posvalue = advancedsentiment(message)[1]
+    negvalue = advancedsentiment(message)[2]
+    """
+    # write to csv file
     # print pos_tagger(messagenourl)
+    # print "%s \t %s  \t %s  \t %s  \t %s \t %s" %(message_no, subjectivity, polarity, classification, posvalue, negvalue)
+
+    # writer.writerow([message_no,subjectivity, polarity, classification, posvalue, negvalue])
+
+    writer.writerow([
+        subj_polar(message)[0],
+        subj_polar(message)[1],
+        advancedsentiment(message)[0],
+        advancedsentiment(message)[1],
+        advancedsentiment(message)[2]
+    ])
+
+
+outfile.close()
+
+
